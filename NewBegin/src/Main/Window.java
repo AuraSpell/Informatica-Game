426ff49b9e6fe6dc280a99dc8394cd911824e4ed
AuraSpell <a_tsui30@hotmail.com>
2016-12-07 12:55:41 +0100

    fghfgh

---
+++ NewBegin/src/Main/Window.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas {
+
+	private static final long serialVersionUID = -24080600533728354L;
+	
+
+}
